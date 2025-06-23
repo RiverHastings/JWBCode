22243e14549d013ff1bbfb3fce86c16ddf8d8ca1
RiverHastings <160418938+RiverHastings@users.noreply.github.com>
2025-06-23 08:10:21 +0530

    Update LConstants.java

--- TeamCode/src/main/java/pedroPathing/constants/LConstants.java
+++ TeamCode/src/main/java/pedroPathing/constants/LConstants.java
@@ -5,7 +5,17 @@ import com.pedropathing.localization.constants.*;
 
 public class LConstants {
     static {
+        DriveEncoderConstants.forwardTicksToInches = 1;
+        DriveEncoderConstants.strafeTicksToInches = 1;
+        DriveEncoderConstants.turnTicksToInches = 1;
         
+        DriveEncoderConstants.robot_Width = 1;
+        DriveEncoderConstants.robot_Length = 1;
+        
+        DriveEncoderConstants.leftFrontEncoderDirection = Encoder.REVERSE;
+        DriveEncoderConstants.rightFrontEncoderDirection = Encoder.FORWARD;
+        DriveEncoderConstants.leftRearEncoderDirection = Encoder.REVERSE;
+        DriveEncoderConstants.rightRearEncoderDirection = Encoder.FORWARD;
     }
 }
 
