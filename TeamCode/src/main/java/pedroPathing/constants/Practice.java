6fcdba9572862c81b6f78a4b2b457f9ed6b78797
RiverHastings <160418938+RiverHastings@users.noreply.github.com>
2025-07-27 17:40:12 +0530

    Update and rename Practice to Practice.java

---
+++ TeamCode/src/main/java/pedroPathing/constants/Practice.java
@@ -0,0 +1,18 @@
+import com.pedropathing.follower.Follower;
+import com.pedropathing.localization.Pose;
+import com.pedropathing.pathgen.BezierCurve;
+import com.pedropathing.pathgen.BezierLine;
+import com.pedropathing.pathgen.Path;
+import com.pedropathing.pathgen.PathChain;
+import com.pedropathing.pathgen.Point;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import pedroPathing.constants.FConstants;
+import pedroPathing.constants.LConstants;
+
+private Path forwardLine;
+
+public void buildPaths(){
+
+}
