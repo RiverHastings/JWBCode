be7d8962c98976d614015960eeddd7e26b47ed19
RiverHastings <160418938+RiverHastings@users.noreply.github.com>
2025-07-27 17:51:19 +0530

    Update Practice.java

--- TeamCode/src/main/java/pedroPathing/constants/Practice.java
+++ TeamCode/src/main/java/pedroPathing/constants/Practice.java
@@ -11,8 +11,44 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import pedroPathing.constants.FConstants;
 import pedroPathing.constants.LConstants;
 
+// Level 1.1: Straight Line
+private final startPose = new Pose(0, 0, Math.toRadians(0));
+private final endPose = new Pose(30, 0, Math.toRadians(0));
+
 private Path forwardLine;
 
 public void buildPaths(){
+  forwardLine = new Path(new BezierLine(new Point(startPose), new Point(endPose))); 
+  forwardLine.setLinearHeadingInterpolation(startPose.getHeading(), endPose.getHeading());
+}
 
+public void autonomousPathUpdate() {
+    switch (pathState) {
+        case 0:
+            follower.followPath(forwardLine);
+            break;
+    }
+}
+
+public void setPathState(int pState) {
+    pathState = pState;
+    pathTimer.resetTimer();
+}
+
+@Override
+public void init() {
+    pathTimer = new Timer();
+    Constants.setConstants(FConstants.class, LConstants.class);
+    follower = new Follower(hardwareMap);
+    follower.setStartingPose(startPose);
+    buildPaths();
+}
+
+@Override
+public void loop() {
+    follower.update();
+    autonomousPathUpdate();
+    telemetry.addData("Path State", pathState);
+    telemetry.addData("Position", follower.getPose().toString());
+    telemetry.update();
 }
